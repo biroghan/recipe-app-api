a38500c1258774734814263b19064f98c8cc03d3
biroghan <biroghlan95@gmail.com>
2021-04-27 12:00:03 +0430

    Added manage userendpoint.

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -9,9 +9,20 @@ from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
     serializer_class = UserSerializer
 
 class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for the user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes =(permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        return self.request.user
